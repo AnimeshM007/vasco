ccf437ba067a14c7b54fc0861cb7400cd9bf85c9
Animesh Mahato <animeshmahato441@gmail.com>
2022-09-15 11:14:14 +0530

    added how it works section

--- src/components/howItWorks/howItWorks.js
+++ src/components/howItWorks/howItWorks.js
@@ -4,13 +4,14 @@ import HowItWorksPoints from "./howItWorksPoints";
 function HowItWorks() {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.heading}>How it works</div>
+      <div className={classes.heading}>How it works?</div>
       <div className={classes.pointsWrapper}>
         <HowItWorksPoints />
         <HowItWorksPoints />
         <HowItWorksPoints />
         <HowItWorksPoints />
       </div>
+      <button className={classes.button}>Plan my trip!</button>
     </div>
   );
 }
